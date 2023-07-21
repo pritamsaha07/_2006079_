c9e49b310f0c5940551f21e43d5f8d54c3fb5328
pritamsaha07 <sahapritam2002@gmail.com>
2023-07-21 17:17:11 +0530

    first commit

--- afford 2/app.js
+++ afford 2/app.js
@@ -1,42 +1,38 @@
+
 const express = require('express');
 const axios = require('axios');
+
 const app = express();
 const PORT = 8008;
 
-
-async function fetchDataFromURL(url) {
-    try {
-      const response = await axios.get(url);
-      return response.data.numbers;
-    } catch (error) {
-      console.error(`Error fetching data from ${url}: ${error.message}`);
-      return [];
-    }
-  }
 app.get('/numbers', async (req, res) => {
-  const urls = req.query.url;
-  if (!urls) {
-    return res.status(400).json({ error: 'No URLs provided' });
-  }
-
-  const urlList = Array.isArray(urls) ? urls : [urls];
-  const fetchDataPromises = urlList.map(fetchDataFromURL);
-
   try {
-   
-    const responseDataList = await Promise.all(fetchDataPromises);
+    const urls = req.query.url;
 
-  
-    const mergedNumbers = [...new Set(responseDataList.flat())].sort((a, b) => a - b);
+    if (!urls || !Array.isArray(urls)) {
+      return res.status(400).json({ error: 'Invalid URL list provided.' });
+    }
+
+    const urlRequests = urls.map(async (url) => {
+      try {
+        const response = await axios.get(url, { timeout: 3000 });
+        return response.data.numbers;
+      } catch (error) {
+        console.error(`Error fetching data from ${url}: ${error.message}`);
+        return [];
+      }
+    });
+
+    const responses = await Promise.all(urlRequests);
+    const mergedNumbers = [...new Set(responses.flat())].sort((a, b) => a - b);
 
     res.json({ numbers: mergedNumbers });
   } catch (error) {
-    console.error(`Error processing URLs: ${error.message}`);
-    res.status(500).json({ error: 'Error processing URLs' });
+    console.error('Error processing request:', error.message);
+    res.status(500).json({ error: 'Internal server error.' });
   }
 });
 
-
 app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
+  console.log(`Number Management Service is running on port ${PORT}`);
 });
